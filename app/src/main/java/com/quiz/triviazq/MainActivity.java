6d66232338af07412971fd3bde9e2b1d765da15b
Anup Kumar Panwar <1anuppanwar@gmail.com>
2018-02-21 19:55:30 +0530

    Release v1.1

--- app/src/main/java/com/quiz/triviazq/MainActivity.java
+++ app/src/main/java/com/quiz/triviazq/MainActivity.java
@@ -57,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent=new Intent(getApplicationContext(), CreateProfile.class);
                 startActivity(intent);
+                finish();
             }
         });
 
